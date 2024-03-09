7473f6031977c07167e6ed7fb75958adbb98c8ef
av-dev2 <av-dev2@aakvatech.com>
2024-03-09 13:31:16 +0300

    fix: typo from 'varify_service_approval_number_for_LRPM' to 'verify_service_approval_number_for_LRPMT'

--- hms_tz/nhif/api/radiology_examination.js
+++ hms_tz/nhif/api/radiology_examination.js
@@ -19,7 +19,7 @@ frappe.ui.form.on("Radiology Examination", {
                     return;
                 }
                 frappe.call({
-                    method: "hms_tz.nhif.api.healthcare_utils.varify_service_approval_number_for_LRPM", 
+                    method: "hms_tz.nhif.api.healthcare_utils.verify_service_approval_number_for_LRPMT", 
                     args: {
                         company: frm.doc.company,
                         approval_number: frm.doc.approval_number,
