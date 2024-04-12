143ef1fba8f7e664752da8ab98ddcdfd9415e395
av-dev2 <av-dev2@aakvatech.com>
2024-04-12 15:45:39 +0300

    feat: therapy automation and controls like other LRPM documents

--- hms_tz/patches/custom_fields/sessions_cancelled_custom_field_on_therapy_plan_detail.py
+++ hms_tz/patches/custom_fields/sessions_cancelled_custom_field_on_therapy_plan_detail.py
@@ -1,6 +1,7 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
+
 def execute():
     fields = {
         "Therapy Plan Detail": [
@@ -10,7 +11,14 @@ def execute():
                 "label": "Sessions Cancelled",
                 "insert_after": "no_of_sessions",
                 "read_only": 1,
-            }
+            },
+            {
+                "fieldname": "therapy_plan_created",
+                "fieldtype": "Check",
+                "label": "Therapy Plan Created",
+                "insert_after": "section_break_20",
+                "read_only": 1,
+            },
         ],
         "Therapy Plan": [
             {
