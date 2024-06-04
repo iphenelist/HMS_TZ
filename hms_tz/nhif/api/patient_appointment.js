ae6e3fa945a60d445e2e2886a971ae7b8a758f57
elius mgani <78465509+av-dev2@users.noreply.github.com>
2024-06-04 11:02:53 +0300

    chore: remove invoiced=1 for insurance patients

--- hms_tz/nhif/api/patient_appointment.js
+++ hms_tz/nhif/api/patient_appointment.js
@@ -819,8 +819,7 @@ const add_btns = (frm) => {
     let filters = {
         name: ["!=", frm.doc.name],
         department: frm.doc.department,
-        status: ["in", ["Open", "Closed"]],
-        invoiced: 1
+        status: ["in", ["Open", "Closed"]]
     }
     if (frm.doc.insurance_subscription) {
         filters.insurance_subscription = frm.doc.insurance_subscription;
