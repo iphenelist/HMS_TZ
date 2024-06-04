d18aa98de0fa95f4046cccd1050efe4f82bf9aea
elius mgani <78465509+av-dev2@users.noreply.github.com>
2024-06-04 11:12:52 +0300

    chore: remove invoiced=1 for insurance patients

--- hms_tz/nhif/api/patient_appointment.py
+++ hms_tz/nhif/api/patient_appointment.py
@@ -512,8 +512,7 @@ def set_follow_up(appointment_doc, method):
     filters = {
         "name": ["!=", appointment_doc.name],
         "department": appointment_doc.department,
-        "status": ["in", ["Open", "Closed"]],
-        "invoiced": 1,
+        "status": ["in", ["Open", "Closed"]]
     }
     if appointment_doc.insurance_subscription:
         filters["insurance_subscription"] = appointment_doc.insurance_subscription
