571b3bde0909e6ea56e38bb0a499773ec8084c52
av-dev2 <av-dev2@aakvatech.com>
2025-03-07 07:23:17 +0300

    chore: update NHIF price package retrieval method and add alert for background job fetch

--- hms_tz/nhif/api/insurance_company.js
+++ hms_tz/nhif/api/insurance_company.js
@@ -11,7 +11,7 @@ var add_get_price_btn = function (frm) {
     if (!frm.doc.insurance_company_name.includes("NHIF")) { return }
     frm.add_custom_button(__('Get NHIF Price Package'), function () {
         frappe.call({
-            method: 'hms_tz.nhif.api.insurance_company.enqueue_get_nhif_price_package',
+            method: 'hms_tz.nhif.nhif_api.price_package.enqueue_get_nhif_price_packages',
             args: { company: frm.doc.company },
             callback: function (data) {
                 if (data.message) {
@@ -19,6 +19,7 @@ var add_get_price_btn = function (frm) {
                 }
             }
         });
+        frappe.show_alert(__('fetch price package via backgroud job'), 5);
     });
     if (!frm.doc.insurance_company_name.includes("NHIF")) { return }
     frm.add_custom_button(__('Only Process NHIF Records'), function () {
