24b5237740c645b6e1aa971f0341fbb21c301900
av-dev2 <av-dev2@aakvatech.com>
2025-03-11 23:59:50 +0300

    chore: update method reference for processing NHIF records in insurance company API

--- hms_tz/nhif/api/insurance_company.js
+++ hms_tz/nhif/api/insurance_company.js
@@ -24,7 +24,7 @@ var add_get_price_btn = function (frm) {
     if (!frm.doc.insurance_company_name.includes("NHIF")) { return }
     frm.add_custom_button(__('Only Process NHIF Records'), function () {
         frappe.call({
-            method: 'hms_tz.nhif.api.insurance_company.process_nhif_records',
+            method: 'hms_tz.nhif.nhif_api.price_package.process_nhif_records',
             args: { company: frm.doc.company },
             callback: function (data) {
                 if (data.message) {
