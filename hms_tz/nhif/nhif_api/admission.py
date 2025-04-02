55b6c62b8c407527d149534393fc9b9f6abccc67
av-dev2 <av-dev2@aakvatech.com>
2025-04-02 19:20:33 +0300

    feat: add NHIF  overstay notification functionality for inpatient records

--- hms_tz/nhif/nhif_api/admission.py
+++ hms_tz/nhif/nhif_api/admission.py
@@ -1,10 +1,18 @@
 import json
 import frappe
 import requests
+from pypika.terms import Not
+from frappe.query_builder import DocType
 from hms_tz.nhif.api.healthcare_utils import get_item_rate
 from hms_tz.nhif.api.inpatient_record import get_last_encounter
-from frappe.utils import get_url_to_form, get_fullname, now_datetime
 from hms_tz.nhif.doctype.nhif_response_log.nhif_response_log import add_log
+from frappe.utils import (
+    get_url_to_form,
+    get_fullname,
+    now_datetime,
+    date_diff,
+    nowdate
+)
 
 
 @frappe.whitelist()
@@ -620,3 +628,130 @@ def transfer_patient(
 
         return data
 
+
+def send_overstay_nofication():
+    settings = frappe.db.get_all("HMS TZ Settings", filters={"enable_nhif_api": 1}, fields=["company"])
+    if len(settings) == 0:
+        return
+    
+    for d in settings:
+        ip_records = get_inpatient_records(d.company)
+
+        settings_doc = frappe.get_cached_doc("HMS TZ Settings", d.company)
+        
+        token = settings_doc.get_nhif_token()
+
+        url = f"{settings_doc.nhifservice_url}/api/Admissions/SendOverstayNotification"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}"
+        }
+
+        for row in ip_records:
+            inpatient_doc = frappe.get_doc("Inpatient Recrod", row.inpatient_id)
+            inpatient_occupancies = inpatient_doc.get("inpatient_occupancies")
+            if len(inpatient_occupancies) == 0:
+                continue
+            
+            last_bed = inpatient_occupancies[-1]
+            notification_days = get_notification_days_per_ward(last_bed.service_unit)
+
+            if inpatient_doc.last_overstay_notification_date and notification_days:
+                days_diff = date_diff(inpatient_doc.last_overstay_notification_date, nowdate())
+                if days_diff and days_diff <= notification_days:
+                    continue
+
+            payload =  {
+                "admissionNo": inpatient_doc.admission_no,
+                "practitionerNo": row.tz_mct_code,
+                "practitionersRemarks": inpatient_doc.admission_instruction or "",
+                "createdBy": get_fullname(inpatient_doc.owner)
+            }
+
+            payload = json.dumps(payload)
+
+            r = requests.request("Get", url, data=payload, headers=headers, timeout=60)
+            if r.status_code != 200:
+                add_log(
+                    request_type="SendOverstayNotification",
+                    request_url=url,
+                    request_header=headers,
+                    request_body=payload,
+                    response_data=r.text,
+                    status_code=r.status_code,
+                    company=settings_doc.name,
+                    ref_doctype=inpatient_doc.doctype,
+                    ref_docname=inpatient_doc.name
+                )
+
+            else:
+                data = json.loads(r.text)
+                add_log(
+                    request_type="SendOverstayNotification",
+                    request_url=url,
+                    request_header=headers,
+                    request_body=payload,
+                    response_data=data,
+                    status_code=r.status_code,
+                    company=settings_doc.name,
+                    ref_doctype=inpatient_doc.doctype,
+                    ref_docname=inpatient_doc.name
+                )
+
+
+def get_inpatient_records(company):
+    ip = DocType("Inpatient Record")
+    pe = DocType("Patient Encounter")
+    hp = DocType("Healthcare Practitioner")
+
+    ip_records = (
+        frappe.qb.from_(ip)
+        .inner_join(pe)
+        .on(pe.inpatient_record == ip.name)
+        .inner_join(hp)
+        .on(pe.practitioner == hp.name)
+        .select(
+            ip.name.as_("inpatient_id"),
+            ip.admission_no,
+            ip.owner,
+            ip.admission_instruction,
+            pe.practitioner,
+            hp.tz_mct_code
+        )
+        .where(
+            Not(ip.status.isin(["Admission Scheduled", "Discharged"]))
+            & (ip.company == company)
+            & (ip.insurance_company.like("NHIF"))
+            & (
+                ip.admission_no.isnotnull() & ip.admission_no != ""
+            )
+            & (pe.appointment == ip.patient_appointment)
+            & (pe.company == company)
+            & (pe.duplicated == 0)
+            & (pe.finalized == 0)
+        )
+    ).run(as_dict=True)
+
+    return ip_records
+
+
+def get_notification_days_per_ward(service_unit):
+    hsu = DocType("Healthcare Service Unit")
+    hsut = DocType("Healthcare Service Unit Type")
+    hwt = DocType("Healthcare Ward Type")
+
+    hwt_data = (
+        frappe.qb.from_(hsu)
+        .inner_join(hsut)
+        .on(hsu.service_unit_type == hsut.name)
+        .inner_join(hwt)
+        .on(hsut.ward_type == hwt.name)
+        .select(
+            hwt.nofication_required_after
+        )
+        .where(
+            hsu.name == service_unit
+        )
+    ).run(as_dict=True)
+
+    return hwt_data[0].nofication_required_after if len(hwt_data) > 0 else None
