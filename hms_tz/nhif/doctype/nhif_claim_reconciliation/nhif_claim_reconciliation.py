55d8de345c3aaf556d5146f0813d16a6f7361618
av-dev2 <av-dev2@aakvatech.com>
2025-04-13 23:45:07 +0300

    feat: update posting date handling and integrate submitted claims retrieval in NHIF claim reconciliation

--- hms_tz/nhif/doctype/nhif_claim_reconciliation/nhif_claim_reconciliation.py
+++ hms_tz/nhif/doctype/nhif_claim_reconciliation/nhif_claim_reconciliation.py
@@ -6,11 +6,11 @@ import json
 import requests
 from frappe.utils import nowdate, flt
 from frappe.model.document import Document
+from hms_tz.nhif.nhif_api.patient_claim import get_submitted_claims
 
 class NHIFClaimReconciliation(Document):
-	def validate(self):
-		if not self.posting_date: 
-			self.posting_date = nowdate()
+	def before_save(self):
+		self.posting_date = nowdate()
 		
 	def validate_reqd_fields(self):
 		for fieldname in ["company", "claim_year", "claim_month"]:
@@ -18,6 +18,7 @@ class NHIFClaimReconciliation(Document):
 				frappe.throw(frappe.bold(f"{fieldname} is required"))
 	
 	def before_submit(self):
-		if self.status == "Pending":
-			frappe.throw("Cannot submit a pending reconciliation")
+		self.posting_date = nowdate()
+		get_submitted_claims(self)
+	
 
