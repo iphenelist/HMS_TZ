49a6bf369f95ed42a113ed01a89768f53072c75c
av-dev2 <av-dev2@aakvatech.com>
2025-04-14 16:02:03 +0300

    feat: add conditional visibility for submit monthly claim button based on claim status

--- hms_tz/nhif/doctype/nhif_claim_reconciliation/nhif_claim_reconciliation.js
+++ hms_tz/nhif/doctype/nhif_claim_reconciliation/nhif_claim_reconciliation.js
@@ -5,6 +5,17 @@ frappe.ui.form.on('NHIF Claim Reconciliation', {
 	refresh: function(frm) {
 		
 	},
+	onload: (frm) => {
+		if (
+			frm.doc.docstatus === 1 &&
+			frm.doc.total_amount_claimed === frm.doc.erp_total_amount_claimed &&
+			frm.doc.number_of_submitted_claims === frm.doc.erp_number_of_submitted_claims
+		) {
+			frm.set_df_property('submit_monthly_claim', 'hidden', 0);
+		} else {
+			frm.set_df_property('submit_monthly_claim', 'hidden', 1);
+		}
+	},
 	submit_monthly_claim: (frm) => {
 		frappe.call({
 			method: "hms_tz.nhif.doctype.nhif_monthly_claim.nhif_monthly_claim.submit_monthly_claim_via_api",
