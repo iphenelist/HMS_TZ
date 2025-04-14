4b3391c8a22e6bb1d30efcaad9f651110403b476
av-dev2 <av-dev2@aakvatech.com>
2025-04-14 14:37:58 +0300

    refactor: remove unused get_detail function from NHIF Claim Reconciliation

--- hms_tz/nhif/doctype/nhif_claim_reconciliation/nhif_claim_reconciliation.js
+++ hms_tz/nhif/doctype/nhif_claim_reconciliation/nhif_claim_reconciliation.js
@@ -4,22 +4,5 @@
 frappe.ui.form.on('NHIF Claim Reconciliation', {
 	refresh: function(frm) {
 		
-	},
-
-	get_detail: (frm) => {
-		if (frm.doc.status == "Pending") {
-			frappe.call({
-				method: "hms_tz.nhif.doctype.nhif_claim_reconciliation.nhif_claim_reconciliation.get_submitted_claims",
-				args: {
-					self: frm.doc
-				},
-				freeze: true,
-				callback: function(r) {
-					if (r.message) {
-						frm.refresh();
-					}
-				}
-			});
-		}
 	}
 });
