108788eaf51fb69d35848af7f228d9f1611919e1
av-dev2 <av-dev2@aakvatech.com>
2025-04-22 15:28:08 +0300

    feat: add functionality to retrieve diseases and enqueue background job for NHIF API

--- hms_tz/nhif/api/insurance_company.js
+++ hms_tz/nhif/api/insurance_company.js
@@ -187,10 +187,21 @@ var add_nhif_actions_btn = function (frm) {
     frm.add_custom_button(__('Get Facilities'), function () {
         frappe.call({
             method: 'hms_tz.nhif.nhif_api.facility.enqueue_get_facilities',
-            args: {
-                company: frm.doc.company,
-                caller: 'Front End'
-            },
+            args: {company: frm.doc.company},
+            freeze: true,
+            freeze_message: __('<i class="fa fa-spinner fa-spin fa-4x"></i>'),
+            callback: function (data) {
+                if (data.message) {
+                    console.log(data.message)
+                }
+            }
+        });
+    }, __('NHIF Actions'));
+
+    frm.add_custom_button(__('Get Diseases'), function () {
+        frappe.call({
+            method: 'hms_tz.nhif.nhif_api.reference.enqueue_get_diseases',
+            args: {company: frm.doc.company},
             freeze: true,
             freeze_message: __('<i class="fa fa-spinner fa-spin fa-4x"></i>'),
             callback: function (data) {
