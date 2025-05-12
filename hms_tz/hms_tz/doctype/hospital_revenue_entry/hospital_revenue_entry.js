21417468755b3a59b473e0da8b52de2f54c585b0
av-dev2 <av-dev2@aakvatech.com>
2025-05-12 11:48:17 +0300

    feat: add Hospital Revenue Entry doctype with initial structure and test case

---
+++ hms_tz/hms_tz/doctype/hospital_revenue_entry/hospital_revenue_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hospital Revenue Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
