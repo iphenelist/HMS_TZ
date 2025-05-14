3a6ceb0b6c903052b9dc2e01cf765e2c499e1fd5
av-dev2 <av-dev2@aakvatech.com>
2025-05-14 18:19:18 +0300

    chore: remove NHIF Cost Sharing doctype and related files

--- hms_tz/nhif/doctype/nhif_cost_sharing/nhif_cost_sharing.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Aakvatech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('NHIF Cost Sharing', {
-	// refresh: function(frm) {
-
-	// }
-});
