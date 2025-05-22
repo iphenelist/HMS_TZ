e27ba1b77b11ae992f1d2ff8429eb6eadbc6433f
av-dev2 <av-dev2@aakvatech.com>
2025-05-22 18:08:37 +0300

    feat: update revenue entry on submit for clinical procedures, delivery notes, lab tests, radiology examinations, and therapy sessions

--- hms_tz/nhif/api/radiology_examination.py
+++ hms_tz/nhif/api/radiology_examination.py
@@ -9,9 +9,12 @@ from frappe import _
 from frappe.query_builder import DocType
 from frappe.utils import get_fullname, nowdate
 
-from hms_tz.hms_tz.doctype.hospital_revenue_entry.hospital_revenue_entry import create_revenue_entry
 from hms_tz.nhif.api.healthcare_utils import create_delivery_note_from_LRPT
 from hms_tz.nhif.api.lab_test import check_cash_payments_from_encounter
+from hms_tz.hms_tz.doctype.hospital_revenue_entry.hospital_revenue_entry import (
+    create_revenue_entry,
+    update_revenue_entry,
+)
 
 
 def after_insert(doc, method):
@@ -53,6 +56,13 @@ def before_submit(doc, method):
 
 def on_submit(doc, method):
     update_radiology_procedure_prescription(doc)
+    update_revenue_entry(
+        "Radiology Examination",
+        doc.name,
+        "Radiology Procedure Prescription",
+        doc.hms_tz_ref_childname,
+        lrpmt_status="Submitted",
+    )
     # create_delivery_note(doc)
 
 
