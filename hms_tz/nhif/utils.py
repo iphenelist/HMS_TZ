d0c754d8554c7d802e2360605933d349563d50fe
av-dev2 <av-dev2@aakvatech.com>
2025-06-08 09:48:19 +0300

    feat: refactor issue_nhif_service to streamline point of care retrieval and enhance data handling

--- hms_tz/nhif/utils.py
+++ hms_tz/nhif/utils.py
@@ -14,7 +14,6 @@ def is_issuance_biometric_verification_enabled(doc_type, company, field):
 def issue_nhif_service(
     ref_doctype,
     ref_docname,
-    point_of_care,
     service_type,
     service_name,
     fingerprint,
@@ -23,29 +22,32 @@ def issue_nhif_service(
     biometric_method="NONE"
 ):
     doc = frappe.get_cached_doc(ref_doctype, ref_docname)
-    
+
     settings_doc = frappe.get_cached_doc("HMS TZ Setting", doc.company)
 
-    point_of_care = get_patient_care_name(service_type, point_of_care)
+    point_of_care = get_patient_care_name(service_type, service_name)
 
-    poc_reference = get_poc_reference_no(
-        point_of_care=point_of_care,
-        practitioner=doc.get("practitioner") or doc.get("healthcare_practitioner"),
-        fingerprint=fingerprint,
-        fpcode=fpcode,
-        biometric_method=biometric_method,
-        company=doc.company,
-        appointment_id=doc.get("appointment") or doc.get("hms_tz_appointment_no"),
-        authorization_no=doc.get("authorization_number") or "",
-        settings_doc=settings_doc,
-        ref_doctype=doc.doctype,
-        ref_docname=doc.name,
-    )
+    data = {}
+    if not doc.poc_reference_no:
+        data = get_poc_reference_no(
+            point_of_care=point_of_care,
+            practitioner=doc.get("practitioner") or doc.get("healthcare_practitioner"),
+            fingerprint=fingerprint,
+            fpcode=fpcode,
+            biometric_method=biometric_method,
+            company=doc.company,
+            appointment_id=doc.get("appointment") or doc.get("hms_tz_appointment_no"),
+            authorization_no=doc.get("authorization_number") or "",
+            settings_doc=settings_doc,
+            ref_doctype=doc.doctype,
+            ref_docname=doc.name,
+        )
 
     if doc.doctype != "Delivery Note":
         if doc.is_restricted == 1 and doc.approval_number:
-            issue_approved_service(
+            d = issue_approved_service(
                 doc,
+                doc.approval_number,
                 service_type,
                 service_name,
                 fingerprint,
@@ -55,6 +57,9 @@ def issue_nhif_service(
                 biometric_method=biometric_method
             )
 
+            if d:
+                data.update(d)
+
     else:
         for row in doc.items:
             if (
@@ -62,9 +67,10 @@ def issue_nhif_service(
                 not row.approval_number
             ):
                 continue
-            
-            issue_approved_service(
+
+            d = issue_approved_service(
                 doc,
+                row.approval_number,
                 service_type,
                 row.item_code,
                 fingerprint,
@@ -75,7 +81,10 @@ def issue_nhif_service(
                 biometric_method=biometric_method
             )
 
-    return poc_reference
+            if d:
+                data.update(d)
+
+    return data
 
 
 def get_patient_care_name(service_type, service_name):
@@ -88,7 +97,7 @@ def get_patient_care_name(service_type, service_name):
     points_of_care = frappe.get_cached_value(
         service_type,
         service_name,
-        "point_of_care"
+        "points_of_care"
     )
 
     if not points_of_care:
@@ -98,4 +107,3 @@ def get_patient_care_name(service_type, service_name):
         )
 
     return points_of_care
-
