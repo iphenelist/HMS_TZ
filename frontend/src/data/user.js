cef63055ddf14cfbc3360e2d4d2ee3be1b6caa98
av-dev2 <emgani@aakvatech.com>
2025-07-03 18:41:49 +0300

    feat: implement session management and login functionality with reactive state

---
+++ frontend/src/data/user.js
@@ -0,0 +1,13 @@
+import router from "@/router"
+import { createResource } from "frappe-ui"
+
+export const userResource = createResource({
+	url: "frappe.auth.get_logged_user",
+	cache: "User",
+	onError(error) {
+		if (error && error.exc_type === "AuthenticationError") {
+			console.error("Authentication error:", error)
+			router.push({ name: "LoginPage" })
+		}
+	},
+})
