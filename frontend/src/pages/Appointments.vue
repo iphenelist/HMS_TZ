8792d1f83025b8a8cf5aeb69975602cd2a9e104e
av-dev2 <emgani@aakvatech.com>
2025-07-03 18:49:08 +0300

    feat: create Appointments page with grid and meta information

---
+++ frontend/src/pages/Appointments.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="appointment-page min-h-screen bg-gray-50">
+    <AppointmentGrid />
+  </div>
+</template>
+
+<script setup>
+import AppointmentGrid from '@/components/appointments/AppointmentGrid.vue'
+import { usePageMeta } from 'frappe-ui'
+
+
+usePageMeta(() => {
+  return {
+    title: 'Appointments - HMS TZ',
+    description: 'Manage appointments and practitioner schedules'
+  }
+})
+</script>
