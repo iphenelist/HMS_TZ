51708350a216cae2d7be6762ed165a3b85c2cea5
av-dev2 <emgani@aakvatech.com>
2025-09-01 21:06:27 +0300

    feat: add Print component for document printing functionality

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -17,6 +17,7 @@ declare module 'vue' {
     Link: typeof import('./src/components/controls/Link.vue')['default']
     PaymentDialog: typeof import('./src/components/appointments/PaymentDialog.vue')['default']
     PractitionerCard: typeof import('./src/components/appointments/PractitionerCard.vue')['default']
+    Print: typeof import('./src/components/controls/Print.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TestComponent: typeof import('./src/components/TestComponent.vue')['default']
