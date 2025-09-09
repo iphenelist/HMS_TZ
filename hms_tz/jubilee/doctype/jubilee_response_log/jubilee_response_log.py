5d1b175d5f9dc67e6cb90d7b73bfb5c82d159c6c
av-dev2 <emgani@aakvatech.com>
2025-09-09 17:49:45 +0300

    feat: add Jubilee Response Log DocType and related files

---
+++ hms_tz/jubilee/doctype/jubilee_response_log/jubilee_response_log.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2025, Aakvatech and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class JubileeResponseLog(Document):
+	pass
+
+
+def add_jubilee_log(
+    request_type,
+    request_url,
+    request_header=None,
+    request_body=None,
+    response_data=None,
+    status_code=None,
+    company=None,
+    ref_doctype=None,
+    ref_docname=None,
+    card_no=None,
+    authorization_no=None,
+):
+    doc = frappe.new_doc("Jubilee Response Log")
+    doc.request_type = str(request_type)
+    doc.request_url = str(request_url)
+    doc.request_header = str(request_header) or ""
+    doc.request_body = str(request_body) or ""
+    doc.response_data = str(response_data) or ""
+    doc.user_id = frappe.session.user
+    doc.status_code = status_code or ""
+    doc.company = company
+    doc.ref_doctype = ref_doctype or ""
+    doc.ref_docname = ref_docname or ""
+    doc.card_no = card_no or ""
+    doc.authorization_no = authorization_no or ""
+    doc.save(ignore_permissions=True)
+    frappe.db.commit()
+    return doc.name
+
