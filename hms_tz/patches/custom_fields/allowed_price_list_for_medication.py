5a52fb8ac27759f7636efba63722bb45514636b0
iphenelist <innocentphenelist@gmail.com>
2025-09-09 17:54:16 +0300

    fix: remove patch file for existing fields in hms tz settings

--- hms_tz/patches/custom_fields/allowed_price_list_for_medication.py
+++ hms_tz/patches/custom_fields/allowed_price_list_for_medication.py
@@ -3,14 +3,6 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
     fields = {
-        "Company": [
-            {
-                "fieldname": "allow_filtered_medication_on_patient_encounter",
-                "fieldtype": "Check",
-                "label": "Allow Filtered Medication on Patient Encounter",
-                "insert_after": "hms_tz_settings_sb",
-                "description": "Allow to filter medication on Patient Encounter based on allowed price list settled on each medication record",
-            }],
         "Medication": [
             {
                 "fieldname": "allowed_price_lists",
