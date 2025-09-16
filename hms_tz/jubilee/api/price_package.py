2768edae430a2ab5f2fd7a587f411a6308bc705c
av-dev2 <emgani@aakvatech.com>
2025-09-16 16:18:26 +0300

    feat: enhance price package synchronization and add package difference tracking

--- hms_tz/jubilee/api/price_package.py
+++ hms_tz/jubilee/api/price_package.py
@@ -54,7 +54,7 @@ def sync_price_package(
     packages,
     company,
     log_name,
-    insurance_provider="Jubilee"
+    # insurance_provider="Jubilee"
 ):
     if len(packages) == 0:
         return
@@ -112,4 +112,90 @@ def create_price_package(packages, company, log_name):
     frappe.db.bulk_insert(
         "Jubilee Price Package", fields=fields, values=data, chunk_size=1000
     )
+    frappe.db.commit()
+    return True
 
+
+def set_package_diff(company):
+    logs = frappe.get_all(
+        "Jubilee Response Log",
+        filters={
+            "request_type": "GetPricePackage",
+            "response_data": ["not in", ["", None]],
+            "company": company,
+        },
+        fields=["name", "response_data"],
+        order_by="creation desc",
+        page_length=2,
+    )
+
+    if len(logs) < 2:
+        return
+    
+    new_price_packages = []
+    changed_price_packages = []
+    deleted_price_packages = []
+
+    current_rec = json.loads(logs[0]["response_data"])
+    previous_rec = json.loads(logs[1]["response_data"])
+
+    current_package = current_rec.get("Description")
+    previous_package = previous_rec.get("Description")
+
+    current_items = {item["ItemCode"]: item for item in current_package}
+    previous_items = {item["ItemCode"]: item for item in previous_package}
+
+    new_price_packages = [item for code, item in current_items.items() if code not in previous_items]
+    deleted_price_packages = [item for code, item in previous_items.items() if code not in current_items]
+    
+    for key, current_item in current_items.items():
+        if key in previous_items:
+            previous_item = previous_items[key]
+            if current_item != previous_item:
+                fields_changed = {
+                    field: {
+                        "current": current_item[field],
+                        "previous": previous_item[field],
+                    }
+                    for field in current_item
+                    if field in previous_item and current_item[field] != previous_item[field]
+                }
+
+                new_row = current_item.copy()
+                new_row["fields_changed"] = fields_changed
+                new_row["previous_item"] = previous_item
+
+                changed_price_packages.append(new_row)
+
+    if (
+        len(changed_price_packages) > 0
+        or len(new_price_packages) > 0
+        or len(deleted_price_packages) > 0
+    ):
+        doc = frappe.new_doc("Jubilee Update")
+
+        add_price_packages_records(doc, changed_price_packages, "Changed")
+        add_price_packages_records(doc, new_price_packages, "New")
+        add_price_packages_records(doc, deleted_price_packages, "Deleted")
+
+        if doc.get("price_package") and len(doc.price_package) > 0:
+            doc.company = company
+            doc.current_log = logs[0].name
+            doc.previous_log = logs[1].name
+            doc.save(ignore_permissions=True)
+
+
+def add_price_packages_records(doc, rec, type):
+    if len(rec) == 0:
+        return
+
+    for e in rec:
+        price_row = doc.append("price_package", {})
+        price_row.itemcode = e.get("ItemCode")
+        price_row.type = type
+        price_row.olditemcode = e.get("OldItemCode")
+        price_row.itemname = e.get("ItemName")
+        price_row.strength = e.get("Strength")
+        price_row.dosage = e.get("Dosage")
+        price_row.unitprice = e.get("UnitPrice")
+        price_row.record = json.dumps(e)
