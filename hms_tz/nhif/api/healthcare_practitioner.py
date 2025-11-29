33e99b568dc3e9974eefa0c91a05eb3decbd9db0
av-dev2 <emgani@aakvatech.com>
2025-11-29 19:43:33 +0300

    feat(NHIF): refactor practitioner login status and attendance logic for improved clarity and functionality

--- hms_tz/nhif/api/healthcare_practitioner.py
+++ hms_tz/nhif/api/healthcare_practitioner.py
@@ -4,36 +4,39 @@ from frappe.utils import getdate, nowdate
 
 
 @frappe.whitelist()
-def get_nhif_practitioner_login_status(company=None):
-    if not company:
-        company = get_default_company()
-
-        if not company:
-            company = frappe.get_cached_value(
-                "Healthcare Practitioner",
-                {"user_id": frappe.session.user},
-                "hms_tz_company",
-            )
-
-    validate_nhif_attandance = frappe.get_cached_value(
-        "HMS TZ Setting",
-        company,
-        "validate_nhif_practitioner_attendance",
-    )
-
-    if not validate_nhif_attandance:
-        return {"show_login": False, "show_logout": False}
-    
-    date_loggedin = frappe.get_cached_value(
+def get_nhif_practitioner_login_status():
+    practitioner_details = frappe.get_cached_value(
         "Healthcare Practitioner",
-        {"hms_tz_company": company, "user_id": frappe.session.user},
-        "date_loggedin_to_nhif",
+        {"user_id": frappe.session.user},
+        ["hms_tz_company", "date_loggedin_to_nhif"],
+        as_dict=True,
     )
+    if not practitioner_details:
+        return {"show_login": False, "show_logout": False}
+
+    hms_tz_details = frappe.get_cached_value(
+        "HMS TZ Setting",
+        practitioner_details.hms_tz_company,
+        ["validate_nhif_practitioner_attendance", "enable_nhif_api"],
+        as_dict=True,
+    )
+
+    if not hms_tz_details.enable_nhif_api:
+        return {"show_login": False, "show_logout": False}
+
+    if not hms_tz_details.validate_nhif_practitioner_attendance:
+        return {"show_login": False, "show_logout": False}
 
     if (
-        not date_loggedin or (getdate(date_loggedin) != getdate(nowdate()))
+        not practitioner_details.date_loggedin_to_nhif or 
+        (
+            getdate(practitioner_details.date_loggedin_to_nhif) != getdate(nowdate())
+        )
     ):
         return {"show_login": True, "show_logout": False}
     
-    elif date_loggedin and (getdate(date_loggedin) == getdate(nowdate())):
+    elif (
+        practitioner_details.date_loggedin_to_nhif and 
+        (getdate(practitioner_details.date_loggedin_to_nhif) == getdate(nowdate()))
+    ):
         return {"show_login": False, "show_logout": True}
