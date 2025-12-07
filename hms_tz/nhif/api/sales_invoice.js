1bf8e3bb5ccadf0912895a567ba1c2e81b6d4cdd
av-dev2 <emgani@aakvatech.com>
2025-12-07 22:25:58 +0300

    feat(SalesInvoice): remove 'Get Sales Order' button to streamline UI for healthcare services

--- hms_tz/nhif/api/sales_invoice.js
+++ hms_tz/nhif/api/sales_invoice.js
@@ -32,23 +32,6 @@ frappe.ui.form.on("Sales Invoice", {
         frm.add_custom_button(__("Get Healthcare Services"), function () {
           get_healthcare_services_to_invoice(frm);
         });
-        frm.add_custom_button(__("Get Sales Order"), function () {
-          erpnext.utils.map_current_doc({
-            method:
-              "erpnext.selling.doctype.sales_order.sales_order.make_sales_invoice",
-            source_doctype: "Sales Order",
-            target: me.frm,
-            setters: {
-              customer: me.frm.doc.customer || undefined,
-            },
-            get_query_filters: {
-              docstatus: 1,
-              status: ["not in", ["Closed", "On Hold"]],
-              per_billed: ["<", 99.99],
-              company: me.frm.doc.company,
-            },
-          });
-        });
         frm.add_custom_button(
           __("Healthcare Services"),
           function () {
