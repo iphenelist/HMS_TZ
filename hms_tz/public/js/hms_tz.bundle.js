8f886ebaadf2dd37b4af8ea3b15770570ee0d616
av-dev2 <emgani@aakvatech.com>
2026-01-13 01:57:10 +0300

    feat(bundle): Add request_approval module to the main bundle

--- hms_tz/public/js/hms_tz.bundle.js
+++ hms_tz/public/js/hms_tz.bundle.js
@@ -2,3 +2,4 @@ import "./fingerprint/digitalPersona/digitalpersona-shims.js";
 import "./fingerprint/fingerprint.js";
 import "./facial/facial_recognition.js";
 import "./signature/signature.js";
+import "./request_approval.js";
