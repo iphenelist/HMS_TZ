31c1904a3743acfce51bb652bf85cf76a862602f
av-dev2 <emgani@aakvatech.com>
2026-05-08 17:26:41 +0300

    fix: apply user permissions to company filter in nurse roster and auto-select single permitted company
    (cherry picked from commit 1ea5685eb004d111055a464ac73deb657595dbad)

--- hms_tz/public/frontend/assets/InsertLink-C7Ja83qa.js
+++
@@ -1,2 +0,0 @@
-import{E as t,O as s,P as a,Q as i}from"./index-BRag1523.js";const c=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{c as default};
-//# sourceMappingURL=InsertLink-C7Ja83qa.js.map
